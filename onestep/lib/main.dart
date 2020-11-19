2afa04442c746bd156edb3541c4d7ccfa7d418e1
honghoker <ekflzhftk@naver.com>
2020-11-19 12:10:01 +0900

    update

--- onestep/lib/main.dart
+++ onestep/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'login/LoginPage.dart';
 
@@ -7,7 +8,10 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(
-    MyApp(),
+    MultiProvider(
+      providers: [],
+      child: MyApp(),
+    ),
   );
 }
 

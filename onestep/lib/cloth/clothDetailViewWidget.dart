0c40608bb7f7e81c1bf0344a191dca77215231fa
ungchun <leedool3003@naver.com>
2021-01-14 00:51:38 +0900

    search clear & kakao link

--- onestep/lib/cloth/clothDetailViewWidget.dart
+++ onestep/lib/cloth/clothDetailViewWidget.dart
@@ -4,6 +4,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import 'package:intl/intl.dart';
+import 'package:kakao_flutter_sdk/link.dart';
 import 'package:onestep/cloth/imageFullViewerWIdget.dart';
 import 'package:onestep/moor/moor_database.dart';
 import 'package:provider/provider.dart';
@@ -17,12 +18,73 @@ class ClothDetailViewWidget extends StatefulWidget {
   _ClothDetailViewWidgetState createState() => _ClothDetailViewWidgetState();
 }
 
+// kakao test
+class KakaoShareManager {
+  static final KakaoShareManager _manager = KakaoShareManager._internal();
+
+  factory KakaoShareManager() {
+    return _manager;
+  }
+
+  KakaoShareManager._internal() {
+    // 초기화 코드
+    initializeKakaoSDK();
+  }
+
+  void initializeKakaoSDK() {
+    String kakaoAppKey = "c9095cdfce8884adb0b88729a7e95aba";
+    KakaoContext.clientId = kakaoAppKey;
+  }
+
+  Future<bool> isKakaotalkInstalled() async {
+    bool installed = await isKakaoTalkInstalled();
+    return installed;
+  }
+
+  void shareMyCode() async {
+    try {
+      var template = _getTemplate();
+      var uri = await LinkClient.instance.defaultWithTalk(template);
+      await LinkClient.instance.launchKakaoTalk(uri);
+    } catch (error) {
+      print(error.toString());
+    }
+  }
+
+  DefaultTemplate _getTemplate() {
+    String title = "안녕하세여";
+    Uri imageLink = Uri.parse(
+        "http://mud-kage.kakao.co.kr/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png");
+    Link link = Link(
+        webUrl: Uri.parse("https://developers.kakao.com"),
+        mobileWebUrl: Uri.parse("https://developers.kakao.com"));
+
+    Content content = Content(
+      title,
+      imageLink,
+      link,
+    );
+
+    FeedTemplate template = FeedTemplate(content,
+        social: Social(likeCount: 286, commentCount: 45, sharedCount: 845),
+        buttons: [
+          Button("웹으로 보기",
+              Link(webUrl: Uri.parse("https://developers.kakao.com"))),
+          Button("앱으로 보기",
+              Link(webUrl: Uri.parse("https://developers.kakao.com"))),
+        ]);
+
+    return template;
+  }
+}
+
 class _ClothDetailViewWidgetState extends State<ClothDetailViewWidget> {
   List _imageItem = new List();
 
   @override
   void initState() {
     _imageItem.addAll(jsonDecode(widget.product.images));
+
     super.initState();
   }
 
@@ -259,6 +321,16 @@ class _ClothDetailViewWidgetState extends State<ClothDetailViewWidget> {
             icon: new Icon(Icons.share),
             onPressed: () => {
               print("share"),
+              // //kakato test
+              KakaoShareManager().isKakaotalkInstalled().then((installed) {
+                if (installed) {
+                  print("kakao success");
+                  KakaoShareManager().shareMyCode();
+                } else {
+                  print("kakao error");
+                  // show alert
+                }
+              }),
             },
           ),
           new IconButton(
--- onestep/lib/home/homeSearchResultPage.dart
+++ onestep/lib/home/homeSearchResultPage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:onestep/moor/moor_database.dart';
+import 'package:provider/provider.dart';
 
 class HomeSearchResultPage extends StatefulWidget {
   @override
@@ -8,6 +10,11 @@ class HomeSearchResultPage extends StatefulWidget {
 class _HomeSearchResultPageState extends State<HomeSearchResultPage> {
   String tempSearchValue = "";
   TextEditingController _textController;
+  bool _isSearchMode = true;
+  bool _isAutoFocus = true;
+  Search search;
+
+  FocusNode myFocusNode;
 
   @override
   void initState() {
@@ -15,11 +22,76 @@ class _HomeSearchResultPageState extends State<HomeSearchResultPage> {
     _textController = TextEditingController(text: tempSearchValue);
     _textController.selection = TextSelection.fromPosition(
         TextPosition(offset: _textController.text.length));
+
+    myFocusNode = FocusNode();
+  }
+
+  Widget searchBody() {
+    SearchsDao p = Provider.of<AppDatabase>(context).searchsDao;
+
+    return Padding(
+      padding: const EdgeInsets.only(top: 10),
+      child: StreamBuilder<List<Search>>(
+        stream: p.watchSearchs(),
+        builder: (BuildContext context, AsyncSnapshot<List<Search>> snapshot) {
+          if (snapshot.data == null) return new Text(""); // 이거 안넣어주면 오류남
+          // if (snapshot.hasError) {
+          //   print(snapshot.error);
+          //   return new Text('Error: ${snapshot.error}');
+          // }
+          SizedBox(
+            height: 5,
+          );
+          return SizedBox(
+            height: 50,
+            child: ListView.builder(
+              itemCount: snapshot.data.length,
+              physics: ClampingScrollPhysics(),
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (BuildContext context, int index) {
+                return Card(
+                  color: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  child: InkWell(
+                    child: Padding(
+                      padding: EdgeInsets.only(
+                        left: 10,
+                        right: 10,
+                        top: 5,
+                        bottom: 5,
+                      ),
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Text(
+                            "${snapshot.data[snapshot.data.length - (index + 1)].title}",
+                            style: TextStyle(),
+                            textAlign: TextAlign.center),
+                      ),
+                    ),
+                    onTap: () {
+                      print("searchClick");
+                      setState(() {
+                        _isSearchMode = false;
+                        _textController.text = snapshot
+                            .data[snapshot.data.length - (index + 1)].title;
+                      });
+                    },
+                  ),
+                );
+              },
+            ),
+          );
+        },
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     // final TextEditingController _textController = TextEditingController(text: tempSearchValue);
+    SearchsDao p = Provider.of<AppDatabase>(context).searchsDao;
 
     return DefaultTabController(
       length: 2,
@@ -27,72 +99,142 @@ class _HomeSearchResultPageState extends State<HomeSearchResultPage> {
         appBar: AppBar(
           backgroundColor: Colors.white,
           iconTheme: IconThemeData(color: Colors.black),
-          title: Container(
-            width: 300,
-            height: 50,
-            child: TextField(
-              // autofocus: true,
-              // focusNode: FocusNode(canRequestFocus: true),
-              // key: UniqueKey(),
-              controller: _textController,
-
-              onChanged: (text) {
-                setState(() {
-                  tempSearchValue = text;
-                });
-              },
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black)),
-                  prefixIcon: Padding(
-                    padding: const EdgeInsets.only(bottom: 3),
-                    child: Icon(Icons.search),
+          title: Row(
+            children: [
+              // Flexible : overFlowd pixels -> 화면 크기 넘어가는거 막아줌
+              Flexible(
+                child: Container(
+                  width: 300,
+                  height: 50,
+                  child: TextField(
+                    onTap: () {
+                      print("textOnTap");
+                      setState(() {
+                        _isSearchMode = true;
+                        // _isAutoFocus = true;
+                      });
+                    },
+                    // focusNode: myFocusNode,
+                    autofocus: _isSearchMode == true ? true : false,
+                    controller: _textController,
+                    onSubmitted: (text) {
+                      print("search $text");
+                      search = Search(title: text, id: null);
+                      setState(() {
+                        _isSearchMode = false;
+                        // p.insertSearch(search);
+                        text != "" ? p.insertSearch(search) : null;
+                        // _isAutoFocus = false;
+                      });
+                    },
+                    onChanged: (text) {
+                      setState(() {
+                        tempSearchValue = text;
+                      });
+                    },
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black)),
+                        prefixIcon: Padding(
+                          padding: const EdgeInsets.only(bottom: 3),
+                          child: Icon(Icons.search),
+                        ),
+                        // icon: Icon(Icons.search),
+                        suffixIcon: _textController.text != ""
+                            ? IconButton(
+                                icon: Icon(Icons.clear),
+                                onPressed: () {
+                                  setState(() {
+                                    _textController.clear();
+                                  });
+                                },
+                              )
+                            : null,
+                        hintText: "장터나 게시판을 검색해보세요!"),
                   ),
-                  // icon: Icon(Icons.search),
-                  suffixIcon: _textController.text != ""
-                      ? IconButton(
-                          icon: Icon(Icons.clear),
-                          onPressed: () {
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: Container(
+                    child: Text("취소", style: TextStyle(fontSize: 15)),
+                  ),
+                ),
+              )
+            ],
+          ),
+          bottom: _isSearchMode == false
+              ? TabBar(
+                  tabs: [
+                    Tab(
+                      child: Text(
+                        "장터",
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                    Tab(
+                      child: Text(
+                        "게시판",
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                  ],
+                )
+              : null,
+        ),
+        body: _isSearchMode == false
+            ? TabBarView(
+                children: [
+                  Center(
+                    child: FlatButton(
+                      onPressed: () {
+                        // print("myFocusNode");
+                        // myFocusNode.requestFocus();
+                      },
+                      child: Text("장터"),
+                    ),
+                  ),
+                  Center(
+                      child: Text(
+                    "게시판",
+                    style: TextStyle(color: Colors.black),
+                  )),
+                ],
+              )
+            : Column(
+                // mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(15, 15, 15, 0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          child: Text("최근 검색어"),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            Provider.of<AppDatabase>(context, listen: false)
+                                .searchsDao
+                                .deleteAllSearch();
                             setState(() {
                               _textController.clear();
                             });
                           },
-                        )
-                      : null,
-                  hintText: "장터나 게시판을 검색해보세요!"),
-            ),
-          ),
-          bottom: TabBar(
-            tabs: [
-              Tab(
-                child: Text(
-                  "장터",
-                  style: TextStyle(color: Colors.black),
-                ),
+                          child: Container(
+                            child: Text("모두 삭제"),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  searchBody(),
+                ],
               ),
-              Tab(
-                child: Text(
-                  "게시판",
-                  style: TextStyle(color: Colors.black),
-                ),
-              ),
-            ],
-          ),
-        ),
-        body: TabBarView(
-          children: [
-            Center(
-                child: Text(
-              "장터",
-              style: TextStyle(color: Colors.black),
-            )),
-            Center(
-                child: Text(
-              "게시판",
-              style: TextStyle(color: Colors.black),
-            )),
-          ],
-        ),
       ),
     );
   }

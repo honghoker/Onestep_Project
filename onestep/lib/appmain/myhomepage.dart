fe417647a180caf5dcf70b7bd5e825ad8477bc60
monocsp <55018831+monocsp@users.noreply.github.com>
2021-01-16 05:35:28 +0900

    Update myhomepage.dart

--- onestep/lib/appmain/myhomepage.dart
+++ onestep/lib/appmain/myhomepage.dart
@@ -33,7 +33,7 @@ class _MyHomePageState extends State<MyHomePage> {
         productProvider: productProvider,
       ),
     ),
-    BoardMain(),
+    Boardmain(),
 //    NotificationMain(),
     ScrollableTabsDemo(),
     MyinfoWidget(),

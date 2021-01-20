b557e7296a76e896eb192d5b46598f31529ada98
honghoker <ekflzhftk@naver.com>
2021-01-20 23:17:40 +0900

    장터 내 게시글 채팅 막기

--- onestep/lib/cloth/clothDetailViewWidgetcopy.dart
+++ onestep/lib/cloth/clothDetailViewWidgetcopy.dart
@@ -316,6 +316,37 @@ class _ClothDetailViewWidgetcopyState extends State<ClothDetailViewWidgetcopy> {
     }
   }
 
+  Widget bottomChatWidget() {
+    return Padding(
+      padding: EdgeInsets.only(right: 10.0),
+      child: SizedBox(
+        width: 150,
+        child: RaisedButton(
+          onPressed: () {
+            // FirebaseChatController()
+            //     .createChatingRoomToFirebaseStorage(
+            //   false,
+            //   widget.product.firestoreid,
+            //   widget.product.title,
+            //   FirebaseApi.getId(),
+            //   widget.product.uid,
+            // );
+            NotificationManager.navigateToChattingRoom(
+              context,
+              FirebaseApi.getId(),
+              widget.product.uid,
+              widget.product.firestoreid,
+            );
+            //Navigator.of(context).pop();
+          },
+          color: Colors.pink,
+          textColor: Colors.white,
+          child: Text('채팅'),
+        ),
+      ),
+    );
+  }
+
   Widget bottomNavigator() {
     return SizedBox(
       height: 70,
@@ -356,34 +387,7 @@ class _ClothDetailViewWidgetcopyState extends State<ClothDetailViewWidgetcopy> {
               ),
             ),
             Expanded(child: Container()),
-            Padding(
-              padding: EdgeInsets.only(right: 10.0),
-              child: SizedBox(
-                width: 150,
-                child: RaisedButton(
-                  onPressed: () {
-                    // FirebaseChatController()
-                    //     .createChatingRoomToFirebaseStorage(
-                    //   false,
-                    //   widget.product.firestoreid,
-                    //   widget.product.title,
-                    //   FirebaseApi.getId(),
-                    //   widget.product.uid,
-                    // );
-                    NotificationManager.navigateToChattingRoom(
-                      context,
-                      FirebaseApi.getId(),
-                      widget.product.uid,
-                      widget.product.firestoreid,
-                    );
-                    //Navigator.of(context).pop();
-                  },
-                  color: Colors.pink,
-                  textColor: Colors.white,
-                  child: Text('채팅'),
-                ),
-              ),
-            )
+            if (widget.product.uid != FirebaseApi.getId()) bottomChatWidget(),
           ],
         ),
       ),

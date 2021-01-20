b9dede80cee703f2e2e757f2c4e949fcc3ed04a3
kms <ik1643@naver.com>
2021-01-20 16:43:37 +0900

    일반 게시판

--- onestep/lib/notification/chatpage/inBoardChattingRoom.dart
+++ onestep/lib/notification/chatpage/inBoardChattingRoom.dart
@@ -1,16 +1,14 @@
 import 'dart:io';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:extended_image/extended_image.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:onestep/notification/Controllers/firebaseChatController.dart';
 import 'package:onestep/notification/time/chat_time.dart';
 import 'package:onestep/notification/widget/FullmageWidget.dart';
 import 'package:intl/intl.dart';
-
-import 'Controllers/firebaseChatController.dart';
 
 class InBoardChattingRoomPage extends StatelessWidget {
   final String myUid;
